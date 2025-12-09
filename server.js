e00d081d83ba9f319d12b98d2d32c9ba3916823f
Saucyyy8 <mailsuhas.madhu@gmail.com>
2025-12-10 00:04:28 +0530

    Increase payload limit to 100mb for large images

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // Health check
 app.get('/', (req, res) => {
